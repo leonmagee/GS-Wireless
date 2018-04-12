90381ff89f5b556189effc2342df239d5fc73755
Leon Magee <leonmagee33@gmail.com>
2018-04-12 15:49:28 -0700

    update

--- wp-content/themes/gs-wireless/assets/svg/icon-square.php
+++
@@ -1,8 +0,0 @@
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="400px"
-	 height="400px" viewBox="0 0 400 400" enable-background="new 0 0 400 400" xml:space="preserve">
-<g id="square">
-		<rect x="74.999" y="75" transform="matrix(0.7071 0.7071 -0.7071 0.7071 200.0002 -82.8428)" display="inline" fill="#FFFFFF" stroke="#000000" stroke-width="13" stroke-miterlimit="10" width="250.002" height="250"/>
-	
-		<rect x="111.764" y="111.765" transform="matrix(0.7071 0.7071 -0.7071 0.7071 200.0005 -82.8427)" display="inline" fill="#FFFFFF" stroke="#000000" stroke-width="13" stroke-miterlimit="10" width="176.471" height="176.469"/>
-</g>
-</svg>
