099a11c819c347824669d035024975f3044433a6
Leon Magee <leonmagee33@gmail.com>
2018-04-18 09:28:13 -0700

    update

--- wp-content/themes/gs-wireless/functions.php
+++ wp-content/themes/gs-wireless/functions.php
@@ -256,7 +256,7 @@ function GS_Wireless_custom_scripts() {
 	//wp_enqueue_script( 'foundation-init-js' );
 
 	
-	wp_register_style( 'gs-wireless-styles', get_template_directory_uri() . '/assets/css/main.min.css', '', '1.0.1' );
+	wp_register_style( 'gs-wireless-styles', get_template_directory_uri() . '/assets/css/main.min.css', '', '1.0.2' );
 	
 	wp_enqueue_style( 'gs-wireless-styles' );
 
