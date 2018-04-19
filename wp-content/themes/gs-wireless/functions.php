ed4a9459c207bbe1640448cc8bddb5f078feb256
Leon Magee <leonmagee33@gmail.com>
2018-04-19 09:49:35 -0700

    update

--- wp-content/themes/gs-wireless/functions.php
+++ wp-content/themes/gs-wireless/functions.php
@@ -163,18 +163,18 @@ add_action( 'widgets_init', 'GS_Wireless_widgets_init' );
 /**
  * Enqueue scripts and styles.
  */
-function GS_Wireless_scripts() {
-	wp_enqueue_style( 'gs-wireless-style', get_stylesheet_uri() );
+// function GS_Wireless_scripts() {
+// 	wp_enqueue_style( 'gs-wireless-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'gs-wireless-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+// 	wp_enqueue_script( 'gs-wireless-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'gs-wireless-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+// 	wp_enqueue_script( 'gs-wireless-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'GS_Wireless_scripts' );
+// 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+// 		wp_enqueue_script( 'comment-reply' );
+// 	}
+// }
+//add_action( 'wp_enqueue_scripts', 'GS_Wireless_scripts' );
 
 /**
  * Implement the Custom Header feature.
