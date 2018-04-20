3c7bafa9d683f0c5a39bee28c074695b24c45577
Leon Magee <leonmagee33@gmail.com>
2018-04-19 19:34:53 -0700

    update

--- wp-content/themes/gs-wireless/header.php
+++ wp-content/themes/gs-wireless/header.php
@@ -98,25 +98,25 @@
 
 									<ul class="sub-menu">
 										<li>
-											<a href="https://h2odirectnow.com">H2O Direct Log-In</a>
+											<a target="_blank" href="https://h2odirectnow.com">H2O Direct Log-In</a>
 										</li>
 										<li>
-											<a href="https://gsposa.instapayportal.com/login">GS Posa Log-In</a>
+											<a target="_blank" href="https://gsposa.instapayportal.com/login">GS Posa Log-In</a>
 										</li>
 										<li>
-											<a href="https://pos.lycamobile.us/Login/POSLogin.aspx">Lyca Dealer Log-In</a>
+											<a target="_blank" href="https://pos.lycamobile.us/Login/POSLogin.aspx">Lyca Dealer Log-In</a>
 										</li>
 										<li>
-											<a href="https://dsm.lycamobile.us">Lyca Agent Log-In</a>
+											<a target="_blank" href="https://dsm.lycamobile.us">Lyca Agent Log-In</a>
 										</li>
 										<li>
-											<a href="https://gs-wireless-h2o-ss.simtrackmanager.com/wp-login.php">STM H2O SS Log-In</a>
+											<a target="_blank" href="https://gs-wireless-h2o-ss.simtrackmanager.com/wp-login.php">STM H2O SS Log-In</a>
 										</li>
 										<li>
-											<a href="https://gs-wireless-agents.simtrackmanager.com/wp-login.php">STM Agent Log-In</a>
+											<a target="_blank" href="https://gs-wireless-agents.simtrackmanager.com/wp-login.php">STM Agent Log-In</a>
 										</li>
 										<li>
-											<a href="https://gs-wireless-dealers.simtrackmanager.com/wp-login.php">STM Dealer Log-In</a>
+											<a target="_blank" href="https://gs-wireless-dealers.simtrackmanager.com/wp-login.php">STM Dealer Log-In</a>
 										</li>
 
 									</ul>
